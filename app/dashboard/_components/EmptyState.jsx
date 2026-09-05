9f9fb4a9091d0bd1aaf01cf60db660ed26cd809a
Benjilo12 <benjamindarteyy@gmail.com>
2026-09-05 17:41:56 +0100

    Fix button background color in EmptyState component

--- app/dashboard/_components/EmptyState.jsx
+++ app/dashboard/_components/EmptyState.jsx
@@ -7,7 +7,7 @@ function EmptyState() {
     <div className="p-5 py-24 flex items-center flex-col mt-10 border-2 border-dashed border-gray-300 dark:border-gray-600 rounded-lg gap-5">
       <h2>You don't have any short video created</h2>
       <Link href="/dashboard/create-new">
-        <Button className="bg-blue-700 dark:bg-lime-600 dark:text-white">
+        <Button className="bg-blue-600 dark:bg-lime-600 dark:text-white">
           Create New Short Video
         </Button>
       </Link>
