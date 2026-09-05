3f8fc3cbe0214352ac1d7e31c22c5a83fccd1185
Benjilo12 <benjamindarteyy@gmail.com>
2026-09-05 17:37:01 +0100

    Update database connection configuration to include fallback URL

--- configs/db.js
+++ configs/db.js
@@ -3,5 +3,8 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(
+  process.env.DATABASE_URL ||
+    "postgresql://neondb_owner:npg_BwlzQ4hj3vDn@ep-frosty-heart-zaavmpuj-pooler.c-2.eu-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+);
 export const db = drizzle({ client: sql });
